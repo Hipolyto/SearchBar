fc31aa25a0009c8d9b4a0f2a5c3a465c60bb1649
Hipolyto <polo@letsolutions.com.mx>
2017-11-01 12:49:36 -0700

    adding render for serach bar

---
+++ Droid/CustomeNavigationPageRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Android.Support.V7.Graphics.Drawable;
+using Android.Widget;
+using Samples.Droid;
+using SearchBarOnNavBarForms.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android.AppCompat;
+
+
+[assembly: ExportRenderer(typeof(NavigationPage), typeof(CustomeNavigationPageRenderer))]
+namespace Samples.Droid
+{
+    public class CustomeNavigationPageRenderer : NavigationPageRenderer
+    {
+        protected override void OnLayout(bool changed, int l, int t, int r, int b)
+    {
+        base.OnLayout(changed, l, t, r, b);
+        var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+        
+        MainActivity.ToolBar = toolbar;
+        
+        return;
+        
+        for (var i = 0; i < toolbar.ChildCount; i++)
+        {
+            var imageButton = toolbar.GetChildAt(i) as ImageButton;
+
+            var drawerArrow = imageButton?.Drawable as DrawerArrowDrawable;
+            if (drawerArrow == null)
+                continue;
+
+            imageButton.SetImageDrawable(Forms.Context.GetDrawable(Resource.Drawable.abc_btn_check_material));
+        }
+    }
+    }
+}
--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -13,7 +13,7 @@ namespace SearchBarOnNavBarForms.Droid
     [Activity(Label = "SearchBarOnNavBarForms.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        public static Android.Support.V7.Widget.Toolbar ToolBar { get; private set; }     
+        public static Android.Support.V7.Widget.Toolbar ToolBar { get;  set; }     
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
